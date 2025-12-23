a78f3e5ceb5facedef5fdc3c36e50028f476de04
Ilya Grigoriev <ilyagr@users.noreply.github.com>
2025-12-22 21:31:35 -0800

    Attempt 2 at x.jj

--- src/settings.rs
+++ src/settings.rs
@@ -11,6 +11,8 @@ pub enum SettingsError {
     Config(#[from] ConfigError),
     #[error("Shell command error: {0}")]
     Shell(#[from] xshell::Error),
+    #[error("TOML parsing error: {0}")]
+    Toml(#[from] toml::de::Error),
 }
 
 #[derive(Debug, Clone, Deserialize)]
@@ -23,20 +25,23 @@ pub struct Settings {
     pub paths_to_symlink: Vec<PathBuf>,
 }
 
-const JJ_CONFIG_KEY: &str = "x.jw";
-
 impl Settings {
     pub fn new(sh: &Shell) -> Result<Self, SettingsError> {
         // TODO: test As discussed in https://github.com/jj-vcs/jj/pull/8379,
         // this is one way to get a config key as valid TOML.
         let template = r#"name ++ "=" ++ value ++ "\n""#;
-        let jj_config = cmd!(
+        let config_form_jj_str = cmd!(
             sh,
-            "jj config list --include-defaults -T {template} --color=never {JJ_CONFIG_KEY}"
+            "jj config list --include-defaults -T {template} --color=never x.jw"
         )
         .ignore_stderr()
         .read()
         .unwrap_or_default();
+        let config_from_jj: toml::Value = toml::from_str::<toml::Table>(&config_form_jj_str)?
+            .get("x")
+            .and_then(|x| x.get("jw"))
+            .cloned()
+            .unwrap_or(toml::Value::Table(Default::default()));
         let s = Config::builder()
             // Start off by merging in the "default" configuration file
             .add_source(File::from_str(
@@ -47,7 +52,7 @@ impl Settings {
             "#,
                 FileFormat::Toml,
             ))
-            .add_source(File::from_str(&jj_config, FileFormat::Toml))
+            .add_source(config::Config::try_from(&config_from_jj)?)
             .build()?;
         Ok(s.try_deserialize()?)
     }
